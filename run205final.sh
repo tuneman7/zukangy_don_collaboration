60dc64c308757c38afe7701ac2564f9b24492b17
don <tuneman7>
2023-08-12 13:22:14 -0700

    periodic update

--- run205final.sh
+++ run205final.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 cd ./205_final_project/code_files/initial_spark_submit_work/
+
+echo "***************************"
+echo " RUN THIS PROJECT AS ROOT"
+echo "***************************"
+
+sudo su
 . run.sh
