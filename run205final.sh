ef750bc7bb49613cce9e2d6193e8a2f3755348f3
root <tuneman7>
2023-08-12 13:45:49 -0700

    periodic update

--- run205final.sh
+++ run205final.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
+conda deactivate
+deactivate
+if [[ $EUID -ne 0 ]]; then
+    echo "***************************"
+    echo " RUN THIS PROJECT AS ROOT"
+    echo " Switch to root then run command:"
+    echo " . run205final.sh"    
+    echo "***************************"
+    return
+fi
+
 cd ./205_final_project/code_files/initial_spark_submit_work/
 
-echo "***************************"
-echo " RUN THIS PROJECT AS ROOT"
-echo "***************************"
 
-sudo ./run.sh
+
+. run.sh
 
