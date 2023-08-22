47dabe71efc8ddc25388a18bc2df93c2f70f0f3a
Don Irwin <tuneman7@hotmail.com>
2023-08-22 03:47:41 -0700

    Periodic Update

--- run205final.sh
+++ run205final.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 conda deactivate
 deactivate
-if [[ $EUID -ne 0 ]]; then
-    echo "***************************"
-    echo " RUN THIS PROJECT AS ROOT"
-    echo " Switch to root then run command:"
-    echo " . run205final.sh"    
-    echo "***************************"
-    return
-fi
+#if [[ $EUID -ne 0 ]]; then
+#    echo "***************************"
+#    echo " RUN THIS PROJECT AS ROOT"
+#    echo " Switch to root then run command:"
+#    echo " . run205final.sh"    
+#    echo "***************************"
+#    return
+#fi
 
 cd ./205_final_project/code_files/initial_spark_submit_work/
 
