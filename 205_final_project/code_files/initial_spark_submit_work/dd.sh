2104b13789439e98d0f39b34aa3ba512bbecdd5b
don <tuneman7>
2023-08-30 05:21:44 -0700

    periodic update

--- 205_final_project/code_files/initial_spark_submit_work/dd.sh
+++ 205_final_project/code_files/initial_spark_submit_work/dd.sh
@@ -1,5 +1,7 @@
-#!/bin/bash
-
+#mkdir ~/w205/spark-from-files/
+#cd ~/w205/spark-from-files
+#cp ~/w205/course-content/11-Storing-Data-III/docker-compose.yml .
+# cp ~/w205/course-content/11-Storing-Data-III/*.py .
 #bring up images
 docker-compose up -d
 echo "sleeping 25"
