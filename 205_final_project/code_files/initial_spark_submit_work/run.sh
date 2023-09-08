62bb3368d9904a57776467605bab18f1f663c879
don <tuneman7>
2023-09-08 07:00:20 -0700

    periodic update

--- 205_final_project/code_files/initial_spark_submit_work/run.sh
+++ 205_final_project/code_files/initial_spark_submit_work/run.sh
@@ -1,14 +1,9 @@
-#mkdir ~/w205/spark-from-files/
-#cd ~/w205/spark-from-files
-#cp ~/w205/course-content/11-Storing-Data-III/docker-compose.yml .
-# cp ~/w205/course-content/11-Storing-Data-III/*.py .
-#bring up images
 #!/bin/bash
 
 #modify yaml to be environment specific
 #make bash file environment specific.
 python generate_commands_from_templates.py >/dev/null
-
+chmod -R 777 ./
 #shut down docker compose.
 . ./dcd.sh
 #bring the juice
